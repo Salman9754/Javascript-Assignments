1aafd7a0dc623bc6fd6c76bebbe07db3e892ea08
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2024-12-03 12:42:10 +0500

    Assignment 5
    Watch dynamic cards website

---
+++ Js Assignment 5/Watch website/script.js
@@ -0,0 +1,349 @@
+var watches = {
+  ROLEX: {
+    RolexExplorer: {
+      name: "Rolex Explorer",
+      price: "$1000",
+      fullName: "Rolex Explorer – M124270-0001",
+      category: "Men's Watch",
+      caption: "A rugged Oystersteel watch with a black dial, legible hour markers, and waterproof design, ideal for exploration.",
+      Tags: "36mm, Black, CPO, explorer, M124270-0001, Oyster Perpetual, Rolex, sapphire",
+      color: ["golden", "silver"] ,
+      img: "https://iwc.com.pk/wp-content/uploads/2023/02/M124270-0001.jpg"
+    },
+    RolexGMTMasterII: {
+      name: "Rolex GMT Master II",
+      price: "$1500",
+      fullName: "Rolex GMT Master II - 116710BLNR",
+      caption: "A durable Oystersteel watch with black dial, Chromalight display, and GMT function for global travelers.",
+      category: "Men's Watch",
+      color: ["black", "silver"],
+      Tags: "116710BLNR, 40mm, Black, CPO, GMT, Oyster Perpetual, Rolex",
+      img: "https://iwc.com.pk/wp-content/uploads/2022/12/M268622-0002-a.jpg"
+    },
+    RolexOysterLady: {
+      name: "Rolex Oyster Lady",
+      price: "$900",
+      fullName: "ROLEX OYSTER PERPETUAL 26 LADY DATEJUST",
+      caption: "A stylish lady's watch with a classic design, featuring a smooth Oystersteel case and Datejust functionality.",
+      category: "Women's Watch",
+      Tags: "26mm, Datejust, ladydatejust, Oyster Perpetual, Rolex",
+      color: ["Golden", "silver"],
+      img: "https://iwc.com.pk/wp-content/uploads/2021/11/rolex-79178.jpg"
+    },
+    RolexDateJust80: {
+      name: "Rolex Datejust 80",
+      price: "$1200",
+      fullName: "Rolex Datejust 79174-62510B Watch - Silver Jubilee Diamond Dial",
+      caption: "A refined Datejust model with a silver jubilee diamond dial, crafted in Oystersteel for elegance and durability.",
+      category: "Men's Watch",
+      Tags: "79174, 79174-62510B, Rolex, Rolex Datejust",
+      color: "silver",
+      img: "https://iwc.com.pk/wp-content/uploads/2022/05/79174.jpg"
+    },
+    RolexGMT: {
+      name: "Rolex® GMT",
+      price: "$1000",
+      fullName: "Rolex GMT-Master II 126711CHN",
+      category: "Men's Watch",
+      caption: "A sophisticated GMT Master with a two-tone bezel, ideal for global exploration and timekeeping.",
+      color: "silver",
+      Tags: "126711CHNR, 40mm, Men, Oyster Perpetual, Rolex",
+      img: "https://imagedelivery.net/lyg2LuGO05OELPt1DKJTnw/7810e4f8-a105-4b29-7c63-3411e0ce6c00/public"
+    },
+    RolexOyster: {
+      name: "Rolex Oyster",
+      price: "$600",
+      caption: "A versatile Datejust 36 with Champagne index dial, crafted in Oystersteel for a sophisticated look.",
+      fullName: "Rolex Oyster Perpetual Datejust 36 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://imagedelivery.net/lyg2LuGO05OELPt1DKJTnw/f7555c4c-31b2-40fb-72ba-5ebc0a857a00/public"
+    },
+    RolexDateJust28: {
+      name: "Rolex Datejust 28",
+      price: "$1000",
+      category: "Men's Watch",
+      fullName: "Rolex Datejust 28 Diamond Bezel & Index - 69138-8570F",
+      caption: "A graceful Datejust model with diamond bezel and index, designed for refined timekeeping.",
+      color: ["black", "golden", "silver"],
+      Tags: "28mm, 69138-8570F, Automatic, certified pre-owned, Datejust, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2023/02/rolex-79173-62523D-18.jpg"
+    },
+    RolexDateJust79: {
+      name: "Rolex Datejust 79",
+      price: "$1200",
+      fullName: "Rolex Datejust 79174-62510B Watch - Silver Jubilee Diamond Dial",
+      caption: "A refined Datejust model with a silver jubilee diamond dial, crafted in Oystersteel for elegance and durability.",
+      category: "Men's Watch",
+      Tags: "79174, 79174-62510B, Rolex, Rolex Datejust",
+      color: "silver",
+      img: "https://iwc.com.pk/wp-content/uploads/2022/05/79174.jpg"
+    },
+    RolexDateJust36: {
+      name: "Rolex DateJust 36",
+      fullName: "Rolex Datejust 36 – 116200-72600",
+      price: "$700",
+      category: "Men's Watch",
+      caption: "A classic Datejust 36 with a clean design, featuring automatic movement and an Oystersteel case.",
+      Tags: "116200/72600, Automatic, certified, Datejust 36, Datejust 36 Men, pre-owned, Rolex, watch",
+      color: ["black", "golden", "silver"],
+      img: "https://iwc.com.pk/wp-content/uploads/2022/05/Rolex-12.jpg"
+    }
+  },
+
+
+  AUDI: {
+    TamaraRalph: {
+      name: "Tamara Ralph",
+      price: "$2000",
+      caption: "A luxurious Royal Oak Concept Flying Tourbillon with a rose gold case, limited edition design for women.",
+      fullName: "AUDEMARS PIGUET : Royal Oak Concept Flying Tourbillon “Tamara Ralph” Limited Edition",
+      category: "Women's Watch",
+      color: "golden",
+      Tags: "38.5mm Rose Gold-Manual Winding",
+      img: "https://www.aviandco.com/media/catalog/product/cache/1201d8f17a9a86053a029ae3179a3cfc/a/u/audemars-piguet-royal-oak-15412ba-001-1.jpg"
+    },
+    AudemarsPiguet46: {
+      name: "Audemars Piguet 46",
+      price: "$600",
+      caption: "A classic design with champagne index dial, automatic movement, and an elegant 36mm case.",
+      fullName: "Audemars Piguet Perpetual Datejust 36 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://www.aviandco.com/media/catalog/product/cache/82bcb559010dcd84bc6a2f8c2fdad6e6/2/6/26238or-001_1.jpg"
+    },
+    AudemarsPiguet28: {
+      name: "Audemars Piguet 28",
+      price: "$600",
+      caption: "A refined 36mm Datejust with a champagne index dial, perfect for timeless elegance.",
+      fullName: "Audemars Piguet Datejust 36 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://www.aviandco.com/media/catalog/product/cache/1201d8f17a9a86053a029ae3179a3cfc/1/5/15720st_green.jpg"
+    },
+    JumboThin: {
+      name: "Jumbo Thin",
+      price: "$600",
+      caption: "A sleek, automatic movement watch with a champagne index dial and a slim 36mm profile.",
+      fullName: "Audemars Piguet Datejust 36 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://www.aviandco.com/media/catalog/product/cache/1201d8f17a9a86053a029ae3179a3cfc/a/p/ap_royal_oak_26331or_blue-1_1_1.jpg"
+    },
+    AudemarsOyster20: {
+      name: "Audemars Oyster",
+      price: "$600",
+      caption: "A refined Champagne Index Datejust 36 with a classic design, crafted for sophisticated men.",
+      fullName: "Audemars Piguet Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://www.aviandco.com/media/catalog/product/cache/1201d8f17a9a86053a029ae3179a3cfc/1/5/15466bc-002_1.jpg"
+    },
+    AudemarsPiguet21: {
+      name: "Audemars Piguet 21",
+      price: "$600",
+      caption: "A luxurious Datejust with champagne index dial and automatic movement, perfect for every occasion.",
+      fullName: "Audemars Piguet 36 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://www.aviandco.com/media/catalog/product/cache/1201d8f17a9a86053a029ae3179a3cfc/a/p/apro_15416ce_oo_1225ce_01__09_01__f_1.jpg"
+    },
+    AudemarsOyster: {
+      name: "Audemars Oyster",
+      price: "$600",
+      caption: "A refined Champagne Index Datejust 36 with a classic design, crafted for sophisticated men.",
+      fullName: "Audemars Piguet Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://www.aviandco.com/media/catalog/product/cache/1201d8f17a9a86053a029ae3179a3cfc/1/5/15466bc-002_1.jpg"
+    },
+    AudemarsPiguet40: {
+      name: "Audemars Piguet 40",
+      price: "$600",
+      caption: "A sleek 36mm Datejust with champagne index dial, designed for those who appreciate timeless luxury.",
+      fullName: "Audemars Piguet 36 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://www.aviandco.com/media/catalog/product/cache/1201d8f17a9a86053a029ae3179a3cfc/3/8/38.jpg"
+    },
+    AudemarsPiguet22: {
+      name: "Audemars Piguet 22",
+      price: "$600",
+      caption: "A refined automatic Datejust with a champagne index dial, offering timeless style and durability.",
+      fullName: "Audemars Piguet 6 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://www.aviandco.com/media/catalog/product/cache/1201d8f17a9a86053a029ae3179a3cfc/a/p/ap_royal_oak_26331st_black_dial-1_1.jpg"
+    }
+  },
+
+  GShock: {
+    GShockCasio08: {
+      name: "G-Shock Casio 08",
+      price: "$600",
+      caption: "A durable and stylish analog-digital women's watch with a bold black dial and rose gold accents.",
+      fullName: "Casio Analog-Digital Black Dial Women’s Watch-BA-110RG-1ADR (BX157)",
+      category: "Women's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2020/12/BA-110RG-1ADR-1.jpg"
+    },
+    GshockCasio09: {
+      name: "G-shock Casio 09",
+      price: "$600",
+      caption: "A rugged, sporty men's watch with a bold black dial and robust features, perfect for tough environments.",
+      fullName: "G-shock Casio 09 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2022/01/GM-S2100-3ADR-1.jpg"
+    },
+    GshockCasio10: {
+      name: "G-shock Casio 10",
+      price: "$600",
+      caption: "A high-performance men's watch featuring a tough design and stylish black dial with metallic accents.",
+      fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2023/03/GMA-S2100-1ADR.jpg"
+    },
+    GshockCasio16: {
+      name: "G-shock Casio 13",
+      price: "$600",
+      caption: "A strong and reliable men's watch with a bold design, perfect for outdoor adventures and tough conditions.",
+      fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-2100FR-5ADR.jpg"
+    },
+    GshockCasio11: {
+      name: "G-shock Casio 11",
+      price: "$600",
+      caption: "A durable, feature-packed men's watch with a sleek black dial and strong, rugged design.",
+      fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-2000S-1ADR.jpg"
+    },
+    GshockCasio12: {
+      name: "G-shock Casio 12",
+      price: "$600",
+      caption: "A rugged men's watch featuring a black dial and reinforced design, ideal for tough environments.",
+      fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-2100-1A3DR.jpg"
+    },
+    GshockCasio13: {
+      name: "G-shock Casio 13",
+      price: "$600",
+      caption: "A strong and reliable men's watch with a bold design, perfect for outdoor adventures and tough conditions.",
+      fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-2100FR-5ADR.jpg"
+    },
+    GshockCasio14: {
+      name: "G-shock Casio 14",
+      price: "$600",
+      caption: "A rugged men's watch designed for adventure, featuring a durable build and bold black dial.",
+      fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-2200SL-8ADR.jpg"
+    },
+    GshockCasio15: {
+      name: "G-shock Casio 15",
+      price: "$600",
+      caption: "A tough, reliable men's watch designed for extreme conditions, featuring a sleek, black dial.",
+      fullName: "G-shock Casio 10 Champagne Index - 126231-62801",
+      category: "Men's Watch",
+      color: ["golden", "silver"],
+      Tags: "126231-62801, Automatic, certified pre-owned, Datejust 36, Datejust 36 Men, Rolex, watch",
+      img: "https://iwc.com.pk/wp-content/uploads/2023/03/GA-700BP-1ADR.jpg"
+    }
+  },
+
+};
+
+
+var selectedBrand = document.getElementById('brandSelect');
+
+for (var brands in watches) {
+  selectedBrand.innerHTML += `<option value="${brands}">${brands}</option>`
+}
+
+selectedBrand.value = Object.keys(watches)[0];
+
+selectBrand();
+
+function selectBrand(event) {
+  var brand;
+  
+
+  if (event) {
+    brand = event.target.value;  
+  } else {
+    brand = document.getElementById('brandSelect').value;  
+  }
+
+  
+  setTimeout(function() {
+    var brandData = watches[brand];
+    var cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML = ''; 
+
+    for (var models in brandData) {
+      var watch = brandData[models];
+
+      var card = `<div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card watch_card m-auto" style="width: 18rem;">
+                      <img src="${watch.img}" class="card-img-top" alt="..." width="200px" height="200px">
+                      <div class="card-body">
+                        <h5 class="card-title fw-bold" style="color: #162433 !important;">${watch.name}</h5>
+                        <p id="card-text" class="card-text">${watch.caption}</p>
+                        <div class="down d-flex">
+                          <span class="price" style="color: #162433 !important;">${watch.price}</span>
+                          <a href="#" class="btn card-btn" style="background-color: #162433; color: white;">Buy Now</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>`;
+      cardContainer.innerHTML += card;
+    }
+  }, 1000);  
+}
+function showLoader() {
+  Swal.fire({
+    title: 'Loading...',
+    html: 'Please wait while we fetch the data.',
+    allowOutsideClick: false, 
+    showConfirmButton: false,
+    didOpen: () => {
+      Swal.showLoading();
+    },
+  });
+
+ 
+  setTimeout(() => {
+    Swal.close(); 
+    selectBrand();
+  }, 500); 
+}
+
+
